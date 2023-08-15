0aecca968ec579f39b3858b3c2fdbd6ee50d1ef2
igorlix <igorlix.santos@gmail.com>
2023-08-15 18:02:43 -0300

    commit #1

--- Jogo_LPOO/src/upe/poli/games/Main.java
+++ Jogo_LPOO/src/upe/poli/games/Main.java
@@ -39,13 +39,4 @@ public class Main {
 
         scanner.close();
     }
-
-    public static boolean isInteger(String str) {
-        try {
-            Integer.parseInt(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
 }
